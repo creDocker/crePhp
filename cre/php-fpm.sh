170a0ac98841d0da969508c1536e6f26e3eade22
Unknown <michael.kahle@yahoo.de>
2020-04-10 22:17:27 +0200

    Adapt version

--- cre/php-fpm.sh
+++ cre/php-fpm.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash 
 
-if [ $(echo " $PHP_VERSION < 7.2" | bc) -eq 1 ] ; then
-  /usr/sbin/php-fpm7.0 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
-else
-  /usr/sbin/php-fpm7.2 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
-fi
+## if [ $(echo " $PHP_VERSION < 7.2" | bc) -eq 1 ] ; then
+##   /usr/sbin/php-fpm7.0 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
+## else
+##   /usr/sbin/php-fpm7.2 -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
+## fi
 
 #or use if file exists -e 
+
+/usr/sbin/php-fpm$PHP_VERSION -F -O 2>&1 | sed -u 's,.*: \"\(.*\)$,\1,'| sed -u 's,"$,,' 1>&1
